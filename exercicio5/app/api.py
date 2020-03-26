b20bacae589e1e50a7d6d4a5c94a03b1653f8039
wagnergraciano <wagner_graciano0101@hotmail.com>
2020-03-26 18:26:21 -0300

    obj to json working

--- exercicio5/app/api.py
+++ exercicio5/app/api.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource, reqparse
 from .empregado import Empregado
+import json
 
 # connect to db and creates the session
 from sqlalchemy import create_engine,update,delete
@@ -29,11 +30,26 @@ api = Api(app)
 
 class EmpregadoAPI(Resource):
     def get(self):
-        e = Empregado()
-        rst = e.retrieveAll(session)
-        return jsonify({'empregado': rst})
+        f = Empregado()
+        rst = f.retrieveAll(session)
+        rstJson = json.dumps(rst)
+        return jsonify(rst)
+
+class EmpregadoAPIid(Resource):
+    def get(self,id):
+        if session.query(Empregado).get(id):
+            return jsonify({'inlist': True})
+        else:
+            return jsonify({'inlist': False}) 
+
+    # def delete(self, elemento):
+    #     if elemento in lista:
+    #         lista.remove(elemento)
+    #         return jsonify({'sucesso': True})
+    #     else:
+    #         return jsonify({'sucesso': False, 'mensagem':elemento+'nao esta na lista'}) 
 
 api.add_resource(EmpregadoAPI, '/empregado')
-# api.add_resource(EmpregadoAPIid, '/empregado/<id>')
+api.add_resource(EmpregadoAPIid, '/empregado/<id>')
 if __name__ == '__main__':
     app.run(debug=True)
